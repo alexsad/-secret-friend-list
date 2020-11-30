32c3288807791d9250e2d674de1139df495046b0
alexsad <alex.query@gmail.com>
2020-11-30 02:10:35 -0300

    creating a basic page

---
+++ docs/precache-manifest.90883ad1e84770dca21bf3d3dafebbcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36056edcd60d4c929ed1",
+    "url": "/css/app.4eaf20b4.css"
+  },
+  {
+    "revision": "9e4415ec7d5cf9ba22b57c6fd9acd1d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36056edcd60d4c929ed1",
+    "url": "/js/app.a7c671f4.js"
+  },
+  {
+    "revision": "8515b3932a37baf02895",
+    "url": "/js/chunk-vendors.7efaa7af.js"
+  },
+  {
+    "revision": "4058e5b3e0f040b8d54203256d74415f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
